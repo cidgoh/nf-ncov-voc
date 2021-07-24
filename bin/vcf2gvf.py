c045c5c18410a7ae1081bc4d280286941c83fa04
miseminger <madeline.iseminger@gmail.com>
2021-07-23 18:03:43 -0700

    added annotated vcf-to-gvf script and supporting files for functional annotation and clade-defining mutations

---
+++ bin/vcf2gvf.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 23 11:06:22 2021
+
+@author: madeline
+"""
+
+'''
+This script converts VCF files that have been annotated by snpEFF into GVF files, including the functional annotation.
+'''
+
+import pandas as pd
+import re
+import glob
+import os
+import numpy as np
+
+
+gvf_columns = ['#seqid','#source','#type','#start','#end','#score','#strand','#phase','#attributes']
+
+
+def vcftogvf(var_data):
+    
+    df = pd.read_csv(var_data, sep='\t', header=65)
+    df['strain'] = re.search('/(.+?)_ids', var_data).group(1)
+    
+    new_df = pd.DataFrame(index=range(0,len(df)),columns=gvf_columns)
+    
+    #parse EFF column
+    eff_info = df['INFO'].str.findall('\((.*?)\)') #series: extract everything between parentheses as elements of a list
+    eff_info = eff_info.apply(pd.Series)[0] #take first element of list
+    eff_info = eff_info.str.split(pat='|').apply(pd.Series) #split at pipe, form dataframe
+    
+    #hgvs names
+    hgvs = eff_info[3].str.rsplit(pat='c.').apply(pd.Series)
+    hgvs_protein = hgvs[0].str[:-1]
+    hgvs_protein.replace(r'^\s+$', np.nan, regex=True)
+    hgvs_nucleotide = 'c.' + hgvs[1]
+    new_df['#attributes'] = new_df['#attributes'].astype(str) + 'Name=' + hgvs_protein + ';'
+    new_df['#attributes'] = new_df['#attributes'].astype(str) + 'nt_name=' + hgvs_nucleotide + ';'
+    
+    #gene names
+    new_df['#attributes'] = new_df['#attributes'].astype(str) + 'gene=' + eff_info[5] + ';'
+
+    #mutation type: looks like MISSENSE or SILENT from Zohaib's file
+    new_df['#attributes'] = new_df['#attributes'].astype(str) + 'mutation_type=' + eff_info[1] + ';'    
+    
+    #columns copied straight from Zohaib's file
+    for column in ['REF','ALT']:
+        key = column.lower()
+        if key=='ref':
+            key = 'Reference_seq'
+        elif key=='alt':
+            key = 'Variant_seq'
+        new_df['#attributes'] = new_df['#attributes'].astype(str) + key + '=' + df[column].astype(str) + ';'
+
+    #add ao, dp, ro
+    info = df['INFO'].str.split(pat=';').apply(pd.Series) #split at ;, form dataframe
+    new_df['#attributes'] = new_df['#attributes'] + info[5].str.lower() + ';' #ao
+    new_df['#attributes'] = new_df['#attributes'] + info[7].str.lower() + ';' #dp
+    new_df['#attributes'] = new_df['#attributes'] + info[28].str.lower() + ';' #ro
+    
+    #add strain name
+    new_df['#attributes'] = new_df['#attributes'] + 'viral_lineage=' + df['strain'] + ';'
+    
+    #add WHO strain name
+    alt_strain_names = {'B.1.1.7': 'Alpha', 'B.1.351': 'Beta', 'P.1': 'Gamma', 'B.1.617.2': 'Delta', 'B.1.427': 'Epsilon', 'B.1.429': 'Epsilon', 'P.2': 'Zeta', 'B.1.525': 'Eta', 'P.3': 'Theta', 'B.1.526': 'Iota', 'B.1.617.1': 'Kappa'}
+    mapped_alt_strains = df['strain'].map(alt_strain_names)
+    new_df['#attributes'] = new_df['#attributes'] + 'who_label=' + mapped_alt_strains + ';'
+
+    #add VOC/VOI designation
+    if mapped_alt_strains.all() in {'Alpha', 'Beta', 'Gamma', 'Delta'}:
+        new_df['#attributes'] = new_df['#attributes'] + 'status=VOC;'
+    else:
+        new_df['#attributes'] = new_df['#attributes'] + 'status=VOI;'
+    
+    #remove starting NaN; leave trailing ';'
+    new_df['#attributes'] = new_df['#attributes'].str[3:]
+    
+    #fill in other GVF columns
+    new_df['#seqid'] = df['#CHROM']
+    new_df['#source'] = '.'
+    new_df['#type'] = info[40].str.split(pat='=').apply(pd.Series)[1]
+    new_df['#start'] = df['POS']
+    new_df['#end'] = (df['POS'] + df['ALT'].str.len() - 1).astype(str)  #this needs fixing
+    new_df['#score'] = '.'
+    new_df['#strand'] = '+'
+    new_df['#phase'] = '.'
+    
+    new_df = new_df[gvf_columns] #only keep the columns needed for a gvf file
+    
+    return new_df
+
+
+
+#takes 3 arguments: an output file of vcftogvf.py, Anoosha's annotation file from Pokay, and the clade defining mutations tsv.
+def add_functions(gvf_df, annotation_file, clade_file, strain):
+
+    #load files into Pandas dataframes
+    df = pd.read_csv(annotation_file, sep='\t', header=0) #load functional annotations spreadsheet
+    #gvf = pd.read_csv(gvf_df, sep='\t', header=3) #load entire GVF file for modification
+    gvf = gvf_df
+    clades = pd.read_csv(clade_file, sep='\t', header=0, usecols=['strain', 'mutation']) #load entire GVF file for modification
+    clades = clades.loc[clades.strain == strain]
+    attributes = gvf["#attributes"].str.split(pat=';').apply(pd.Series)
+
+    hgvs_protein = attributes[0].str.split(pat='=').apply(pd.Series)[1]
+    hgvs_nucleotide = attributes[1].str.split(pat='=').apply(pd.Series)[1]
+    gvf["mutation"] = hgvs_protein.str[2:] #drop the prefix
+
+
+    #merge annotated vcf and functional annotation files by 'mutation' column in the gvf
+    for column in df.columns:
+        df[column] = df[column].str.lstrip()
+    merged_df = pd.merge(df, gvf, on=['mutation'], how='right') #add functional annotations
+    merged_df = pd.merge(clades, merged_df, on=['mutation'], how='right') #add clade-defining mutations
+
+
+    #collect all mutation groups (including reference mutation) in a column, sorted alphabetically
+    #this is more roundabout than it needs to be; streamline with grouby() later
+    merged_df["mutation_group"] = merged_df["comb_mutation"].astype(str) + ", '" + merged_df["mutation"].astype(str) + "'"
+    mutation_groups = merged_df["mutation_group"].str.split(pat=',').apply(pd.Series)
+    mutation_groups = mutation_groups.apply(lambda s:s.str.replace("'", ""))
+    mutation_groups = mutation_groups.apply(lambda s:s.str.replace(" ", ""))
+    mutation_groups = mutation_groups.transpose() 
+    sorted_df = mutation_groups
+    for column in mutation_groups.columns:
+        sorted_df[column] = mutation_groups.sort_values(by=column, ignore_index=True)[column]
+    sorted_df = sorted_df.transpose()
+    
+    #since they're sorted, put everything back into a single cell, don't care about dropna
+    df3 = sorted_df.apply(lambda x :','.join(x.astype(str)),axis=1)
+    unique_groups = df3.drop_duplicates() #92 unique groups
+    unique_groups_multicol = sorted_df.drop_duplicates() #92 unique groups, not all members of which might be present in the gvf file
+    merged_df["mutation_group_labeller"] = df3 #for sanity checking
+
+    #make a unique id for mutation groups that have all members represented in the vcf
+    #for groups with missing members, delete those functional annotations
+    merged_df["id"] = 'NaN'
+    id_num = 0
+    for row in range(unique_groups.shape[0]):
+        group_mutation_set = set(unique_groups_multicol.iloc[row])
+        group_mutation_set = {x for x in group_mutation_set if (x==x and x!='nan')} #remove nan and 'nan' from set
+        gvf_all_mutations = set(gvf['mutation'].unique())
+        indices = merged_df[merged_df.mutation_group_labeller == unique_groups.iloc[row]].index.tolist()
+        if group_mutation_set.issubset(gvf_all_mutations): #if all mutations in the group are in the vcf file, include those rows and give them an id
+            merged_df.loc[merged_df.mutation_group_labeller == unique_groups.iloc[row], "id"] = "ID_" + str(id_num)
+            id_num += 1
+        else:
+            merged_df = merged_df.drop(indices) #if not, drop group rows, leaving the remaining indices unchanged
+
+    #change semicolons in function descriptions to colons
+    merged_df['function_description'] = merged_df['function_description'].str.replace(';',':')
+    #add key-value pairs to attributes column
+    for column in ['function_category', 'source', 'citation', 'comb_mutation', 'function_description']:
+        key = column.lower()
+        merged_df[column] = merged_df[column].fillna('') #replace NaNs with empty string
+        if column in ['function_category', 'citation', 'function_description']:
+            merged_df["#attributes"] = merged_df["#attributes"].astype(str) + key + '=' + '"' + merged_df[column].astype(str) + '"' + ';'
+        else:
+            merged_df["#attributes"] = merged_df["#attributes"].astype(str) + key + '=' + merged_df[column].astype(str) + ';'
+
+    #change clade-defining attribute to True/False depending on content of 'strain' column
+    merged_df.loc[merged_df.strain == strain, "#attributes"] = merged_df.loc[merged_df.strain == strain, "#attributes"].astype(str)  + "clade_defining=True;"
+    merged_df.loc[merged_df.strain != strain, "#attributes"] = merged_df.loc[merged_df.strain != strain, "#attributes"].astype(str)  + "clade_defining=False;"
+
+    #add ID to attributes
+    merged_df["#attributes"] = 'ID=' + merged_df['id'].astype(str) + ';' + merged_df["#attributes"].astype(str)
+    
+    
+    #get list of names in tsv but not in functional annotations, and vice versa, saved as a .tsv
+    tsv_names = gvf["mutation"].unique()
+    pokay_names = df["mutation"].unique()
+    print(str(np.setdiff1d(tsv_names, pokay_names).shape[0]) + "/" + str(tsv_names.shape[0]) + " mutation names were not found in pokay")
+    in_pokay_only = pd.DataFrame({'in_pokay_only':np.setdiff1d(pokay_names, tsv_names)})
+    in_tsv_only = pd.DataFrame({'in_tsv_only':np.setdiff1d(tsv_names, pokay_names)})
+    leftover_names = in_tsv_only
+    leftover_names["strain"] = strain
+    
+    clade_names = clades["mutation"].unique()
+    leftover_clade_names = pd.DataFrame({'unmatched_clade_names':np.setdiff1d(clade_names, tsv_names)})
+    leftover_clade_names["strain"] = strain
+    
+    return merged_df, leftover_names, gvf["mutation"].tolist(), leftover_clade_names
+    
+    
+
+#process all annotated VCF files in the data folder
+def convertfolder(folderpath):
+    new_folder = folderpath + "/gvf_files" #gvf files from this script will be stored in here
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+    os.chdir(folderpath)
+    
+    parent_directory = os.path.dirname(os.path.dirname(os.getcwd())) #path to voc_prototype main folder
+    annotation_file = parent_directory + '/functional_annotation_V.0.2.tsv'
+    clade_file = parent_directory + '/clade_defining_mutations.tsv'
+
+    pragmas = pd.DataFrame([['##gff-version 3'], ['##gvf-version 1.10'], ['##species NCBI_Taxonomy_URI=http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=2697049']]) #pragmas are in column 0
+
+    print("Processing vcf files in " + folderpath + " ...")
+    print("")
+    
+    for file in glob.glob('./*.vcf'): #get all .tsv files
+        print("input file: ", file)
+        #get strain name
+        pat = r'.*?/(.*)_ids.*'
+        match = re.search(pat, file)
+        strain = match.group(1)
+        #create gvf from annotated vcf
+        gvf = vcftogvf(file)
+        annotated_gvf, leftover_names, mutations, leftover_clade_names = add_functions(gvf, annotation_file, clade_file, strain)
+        annotated_gvf = annotated_gvf[gvf_columns]
+        #add pragmas to df, then save to .tsv
+        annotated_gvf = pd.DataFrame(np.vstack([annotated_gvf.columns, annotated_gvf])) #columns are now 0, 1, ...
+        final_gvf = pragmas.append(annotated_gvf)
+        filepath = "./gvf_files" + file[1:-4] + ".annotated.gvf"
+        print("saved as: ", filepath)
+        print("")
+        final_gvf.to_csv(filepath, sep='\t', index=False, header=False)
+        
+    print("Processing complete.")
+        
+
+
+'''Run script for a given folder of annotated VCFs'''
+folder = "reference_data_/22_07_2021" #folder containing annotated VCFs
+convertfolder(folder)
