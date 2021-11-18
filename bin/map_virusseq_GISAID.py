4130bb7b666fe9e0f0e121f97104b763364e06e0
Muhammad Zohaib Anwar <zohaib_anwar@ymail.com>
2021-11-18 11:49:16 -0800

    Script to map VirusSeq to its Lineages from GISAID

---
+++ bin/map_virusseq_GISAID.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+import csv
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Map VirusSeq data to GISAID Metadata for '
+                    'pangolin')
+    parser.add_argument('--virusseq', type=str, default=None,
+                        help='Metadata file (.tsv) format')
+    parser.add_argument('--gisaid', type=str, default=None,
+                        help='Metadata file (.tsv) format')
+    parser.add_argument('--output', type=str, default=None,
+                        help='Metadata file (.tsv) format')
+
+    return parser.parse_args()
+
+
+def write_metadata(dataframe):
+    dataframe.to_csv(args.output,
+                     sep="\t",
+                     quoting=csv.QUOTE_NONE,
+                     index=False, header=True)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    virus_seq_df = pd.read_csv(args.virusseq, sep="\t",
+                               low_memory=False)
+    print("Number of sequences in VirusSeq Data Portal: ",
+          len(virus_seq_df))
+
+    print("Sequences in VirusSeq Data Portal with GISAID accessions: ",
+          len(virus_seq_df.loc[
+                  virus_seq_df['GISAID accession'].notna()]))
+
+    gisaid_df = pd.read_csv(args.gisaid, sep="\t", low_memory=False)
+    gisaid_df = gisaid_df.loc[gisaid_df['country'] == 'Canada']
+    print("Number of Canadian sequences in GISAID: ", len(gisaid_df))
+
+    df = pd.merge(gisaid_df, virus_seq_df, how='right',
+                  left_on='gisaid_epi_isl',
+                  right_on='GISAID accession', indicator=True)
+
+    df = df.loc[df['_merge'] == 'both']
+    df = df[['strain', 'virus', 'gisaid_epi_isl',
+             'genbank_accession', 'pango_lineage', 'date', 'region',
+             'country', 'division', 'location', 'submitting_lab',
+             'date_submitted', 'fasta header name']]
+    df = df.drop(['strain'], axis=1)
+    df = df.rename(columns={'fasta header name': 'strain'})
+    write_metadata(dataframe=df)
