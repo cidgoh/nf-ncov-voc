cc2e1a2cbbf2253583b2aba8a29f00328946b9e0
miseminger <madeline.iseminger@gmail.com>
2023-09-27 07:59:34 -0700

    add functional annotations based on both Name and alias

--- bin/addfunctions2gvf.py
+++ bin/addfunctions2gvf.py
@@ -54,9 +54,9 @@ def add_pokay_annotations(gvf, annotation_file):
     for column in df.columns:
         df[column] = df[column].str.strip()
         
-    # merge annotated vcf and functional annotation files by 'Name'
+    # merge annotated vcf and functional annotation files by 'Name' and 'alias'
     df = df.rename(columns={"mutation": "Name"})
-    merged_df = pd.merge(df, gvf, on=['Name'], how='right')
+    merged_df = pd.merge(df, gvf, on=['Name', 'alias'], how='right')
     
     # data cleaning
     merged_df['comb_mutation'] = merged_df['comb_mutation'].str.replace(
