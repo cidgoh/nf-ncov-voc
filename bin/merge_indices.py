19520bc11025593e15b655035db5bdef33011310
miseminger <madeline.iseminger@gmail.com>
2024-03-08 02:20:30 -0800

    convert to new Dask

--- bin/merge_indices.py
+++ bin/merge_indices.py
@@ -8,9 +8,9 @@ Created on Fri Feb 16 12:00:04 2024
 This script merges multiple mutation indices into one.
 If an existing mutation index is provided, the new indices are added to it.
 """
-
+import dask
+dask.config.set({'dataframe.query-planning': True})
 import dask.dataframe as dd
-import pandas as pd
 import argparse
 
 
@@ -38,23 +38,17 @@ if __name__ == '__main__':
 
     # if an original index is provided, merge the new indices with it
     if original_index!=None:
-        indices_to_merge = list(original_index) + gvf_indices_list
+        indices_to_merge = [original_index] + gvf_indices_list
     # if an original index is not provided, make an entirely new index
     else:
         indices_to_merge = gvf_indices_list
-    
+    print(indices_to_merge)
     # read all indices into dask df
     ddf = dd.read_csv(indices_to_merge, sep='\t') 
     # fillna to make groupby() work
     ddf = ddf.fillna('n/a')
     # specify which columns to group by
     group_cols = [x for x in ddf.columns if x!='lineages']
-    '''
-    # specify meta df: this throws an error, but the inferred meta works fine
-    schema = {**dict.fromkeys(ddf.columns, 'str'), 'pos':'int'}
-    meta_df = pd.DataFrame(columns=schema.keys()).astype(schema)
-    ddf = ddf.groupby(by=group_cols)['lineages'].apply(','.join, meta=meta_df).reset_index()
-    '''
     # do groupby operation
     ddf = ddf.groupby(by=group_cols)['lineages'].apply(','.join).reset_index()
     # sort by 'pos'
