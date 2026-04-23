a463246df4e286ca684917044b4184b21206b32b
miseminger <madeline.iseminger@gmail.com>
2026-04-23 15:35:36 -0700

    Add hgvs_alias for non-SARS-CoV-2

--- bin/functions.py
+++ bin/functions.py
@@ -606,7 +606,8 @@ def add_alias_names(df, GENE_PROTEIN_POSITIONS_DICT):
     df.loc[:, 'alias'] = 'n/a'
 
     # get list of all NSP, 3CL, and PlPro proteins in the file:
-    alias_mask = (df['gene_symbol'].str.contains("orf1ab")) & (df['mat_pep']!='n/a')
+    #alias_mask = (df['gene_symbol'].str.contains("orf1ab")) & (df['mat_pep']!='n/a')
+    alias_mask = (df['mat_pep']!='n/a')
     nsps_list = sorted(list(set(df[alias_mask]['mat_pep'].tolist())))
     if len(nsps_list) > 0:
         
